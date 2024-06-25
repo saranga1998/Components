1239addb51ef0b529b3fbfec15e33ec8c64005de
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-06-25 18:23:42 +0530

    HTTP-2

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       {/* <PostList/> */}
-      <PostForm/>gi
+      <PostForm/>
     </div>
   )
 }
--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class PostForm extends Component {
     constructor(props) {
@@ -18,7 +19,13 @@ class PostForm extends Component {
     submitHandler = (e) =>{
         e.preventDefault()
         console.log(this.state)
-        alert(this.state)
+        axios.post('https://jsonplaceholder.typicode.com/posts',this.state)
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
     }
     
   render() {
