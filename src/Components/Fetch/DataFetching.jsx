320be13bd156645312da97deeb401e5d2dfebb23
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-06-27 23:03:02 +0530

    UseEffect-data fetching

--- src/Components/Fetch/DataFetching.jsx
+++ src/Components/Fetch/DataFetching.jsx
@@ -3,10 +3,12 @@ import axios from 'axios'
 
 function DataFetching() {
 
-    const [post,setPost] =useState([])
+    const [post,setPost] =useState({})
+    const [id,setId] = useState(1)
+    const [idFromBtnClick,setIdformBtnClick] = useState(1)
 
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromBtnClick}`)
         .then(res=>{
             console.log(res)
             setPost(res.data)
@@ -15,18 +17,21 @@ function DataFetching() {
         .catch(err =>{
             console.log(err)
         })
-    },[])
+    },[idFromBtnClick])
 
-    
+
   return (
     <div>
-      <ul>
+    <input type='text' value={id} onChange={e => setId(e.target.value)}></input>
+    <button type='button' onClick={()=>setIdformBtnClick(id)}>Fetch Post</button>
+        <div>{post.title}</div>
+      {/* <ul>
         {
             post.map(post=>(
                 <li key={post.id}>{post.title}</li>
             ))
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
